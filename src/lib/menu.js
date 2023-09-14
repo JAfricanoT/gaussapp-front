ca286d169f33f86fda5716fa91bdba05a7dca847
JAfricanoT <josefranciscoat.correo@gmail.com>
2023-09-13 20:32:32 -0400

    feat: menu json data

---
+++ src/lib/menu.js
@@ -0,0 +1,14 @@
+export const menu = [
+    {
+        "icon": "test",
+        "title": "Operación 1" 
+    },
+    {
+        "icon": "test",
+        "title": "Operación 2" 
+    },
+    {
+        "icon": "test",
+        "title": "Operación 3" 
+    }
+]
