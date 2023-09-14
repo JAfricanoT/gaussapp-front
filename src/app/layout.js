8440c54f66a9b2d023b8a65d8e9125d85d954265
JAfricanoT <josefranciscoat.correo@gmail.com>
2023-09-13 20:54:00 -0400

    style: intitial structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 import { menu } from '@/lib/menu'
 import Image from 'next/image'
 import styles from './Layout.module.css'
- 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -15,21 +15,24 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <body className={inter.className}>
-        <div className={styles.config}>
-          <nav>
-            {menu.map(operations => (
-              <ul key={operations.title}>
-                <li>
-                  <Image alt={operations.title + ' Icon'} src={'/favicon.ico'} width={100} height={100}/>
-                  <div>{operations.title}</div>
-                </li>
-              </ul>
-            ))}
-          </nav>
-        </div>
-        <div className={styles.results}>
-          {children}
-        </div>
+        <main className={styles.wrapper}>
+          <div className={styles.config}>
+            <nav className={styles.navbar}>
+              {menu.map(operations => (
+                <ul key={operations.title} className={styles.menuWrapper}>
+                  <li className={styles.menu}>
+                    <Image alt={operations.title + ' Icon'} src={'/favicon.ico'} width={100} height={100} />
+                    <div>{operations.title}</div>
+                  </li>
+                </ul>
+              ))}
+            </nav>
+          </div>
+          <div className={styles.result}>
+            <div>hola</div>
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
