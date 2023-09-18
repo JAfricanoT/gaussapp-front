87dae3788b7d297dcc913dcc7809340dcb097388
JAfricanoT <josefranciscoat.correo@gmail.com>
2023-09-18 02:03:45 -0400

    feat: custom hook for fetching data

---
+++ src/lib/useFetch.js
@@ -0,0 +1,24 @@
+'use client'
+
+import { useEffect, useState } from "react"
+
+export default function useFetch (endpoint, options)  {
+    const [data, setData] = useState()
+
+    useEffect(() => {
+        (async () => {
+            try {
+                // Enviar los datos a la API y recibir la respuesta
+                const response = await fetch(endpoint, options)
+                
+                // Recibir los datos desde el servidor como JSON
+                const result = await response.json()
+                setData(result)
+            } catch (err) {
+                console.error(err)
+            }
+        })()
+    }, [])
+
+    return data
+}
